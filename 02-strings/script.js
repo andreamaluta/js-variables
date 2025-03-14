0647287d25f95e3fb3854422dfabd73ff86cd8a6
andreamaluta <andreamaluta@gmail.com>
2025-03-14 14:08:21 +0100

    [UPD] make 2.1 point

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,14 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
+let Name = "Andrea";
 // Stampa il valore di nome nella console.
+console.log(Name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
