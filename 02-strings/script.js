a16c0763ae2122ad3757c6f5e2df39d74e024801
andreamaluta <andreamaluta@gmail.com>
2025-03-14 14:10:16 +0100

    [UPD] make the 2.2 point

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,7 +8,10 @@ console.log(Name);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+let number = 10;
+let text = "ciao"
+let bool = true;
+console.log(number, text, bool)
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
