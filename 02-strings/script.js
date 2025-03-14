14eb18cf197a00a301ced703c91c032e616eb206
andreamaluta <andreamaluta@gmail.com>
2025-03-14 16:40:12 +0100

    [FIX] change name to variable in 2.1 point

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,8 +1,8 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
-let Name = "Andrea";
+let myName = "Andrea";
 // Stampa il valore di nome nella console.
-console.log(Name);
+console.log(myName);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
